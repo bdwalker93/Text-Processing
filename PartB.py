96c20b99daaaf62d6f2a2aa3cbedd7188e8b80e0
bdwalker93 <bdwalker93@yahoo.com>
2017-01-23 23:48:39 -0800

    Working part B

--- PartB.py
+++ PartB.py
@@ -1,9 +1,10 @@
 import re
+from time import clock
 
-# Reads input from the specified file and tokenizes all words within it
-def generateTokenFrequencyMap(file_name: str) -> {str, int}:
+# Reads input from the specified file and tokenizes all words into a map
+def generateTokenFrequencyMap(file_name1: str) -> {str, int}:
     # Opens the specified file
-    file = open(file_name)
+    file = open(file_name1)
 
     # Stores all of the word frequencies in a map
     frequency_map = {}
@@ -24,12 +25,41 @@ def generateTokenFrequencyMap(file_name: str) -> {str, int}:
 
     return frequency_map
 
+# Finds the number of same words between two documents
+def determineIntersections(file_name2: str, frequency_map: {str, int}) -> int:
+    # Total number of file intersections
+    total_intersections = 0
 
-# Prints word frequencies in decreasing order
-def printFrequencies(frequency_map: {str, int}):
-    for k in sorted(frequency_map, key=frequency_map.get, reverse=True):
-        print(k, "->", frequency_map[k])
+    # Opens the specified file
+    file = open(file_name2)
+
+    # We split on anything that is not a comma, letter (case sensitive), or apostrophe
+    for line in file:
+        for token in re.split('[^a-zA-Z0-9\']', line):
+
+            # Multiple split chars in a row cause an empty token to be shown
+            if token:
+                if token.lower() in frequency_map and frequency_map[token.lower()] > 0:
+                    frequency_map[token.lower()] = -1
+                    total_intersections += 1
+
+                    # Prints the common tokens
+                    print(token.lower())
+
+    # Close the input stream
+    file.close()
+
+    return total_intersections
 
 if __name__ == '__main__':
-    # printFrequencies(computeWordFrequencies(tokenize("C:\\Users\\Brett\\Desktop\\example2.txt")))
-    printFrequencies(generateTokenFrequencyMap("smallExample.txt"))
+
+    # Starts the timer
+    start = clock()
+
+    print(determineIntersections("input2.txt", generateTokenFrequencyMap("input1.txt")))
+
+    # Ends the timer
+    end = clock()
+
+    # Displays execution time
+    print("Total Execution Time:", end - start, "sec")
