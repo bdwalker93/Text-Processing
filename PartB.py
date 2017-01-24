7336eaadabd41e4c370637f268085aec15f2d265
bdwalker93 <bdwalker93@yahoo.com>
2017-01-24 01:50:45 -0800

    Part B now implement error handling. Still needs to handle encoding check.

--- PartB.py
+++ PartB.py
@@ -1,65 +1,75 @@
 import re
-from time import clock
+import sys
 
 # Reads input from the specified file and tokenizes all words into a map
 def generateTokenFrequencyMap(file_name1: str) -> {str, int}:
-    # Opens the specified file
-    file = open(file_name1)
+     try:
+        # Opens the specified file
+        file = open(file_name1)
 
-    # Stores all of the word frequencies in a map
-    frequency_map = {}
+        # Stores all of the word frequencies in a map
+        frequency_map = {}
 
-    # We split on anything that is not a comma, letter (case sensitive), or apostrophe
-    for line in file:
-        for token in re.split('[^a-zA-Z0-9\']', line):
+        # We split on anything that is not a comma, letter (case sensitive), or apostrophe
+        for line in file:
+            for token in re.split('[^a-zA-Z0-9\']', line):
 
-            # Multiple split chars in a row cause an empty token to be shown
-            if token:
-                if token.lower() in frequency_map:
-                    frequency_map[token.lower()] += 1
-                else:
-                    frequency_map[token.lower()] = 1
+                # Multiple split chars in a row cause an empty token to be shown
+                if token:
+                    if token.lower() in frequency_map:
+                        frequency_map[token.lower()] += 1
+                    else:
+                        frequency_map[token.lower()] = 1
 
-    # Close the input stream
-    file.close()
+        # Close the input stream
+        file.close()
 
-    return frequency_map
+        return frequency_map
+     except:
+         print("Error! The following file does not exist:", file_name1)
+
 
 # Finds the number of same words between two documents
 def determineIntersections(file_name2: str, frequency_map: {str, int}) -> int:
     # Total number of file intersections
     total_intersections = 0
 
-    # Opens the specified file
-    file = open(file_name2)
+    try:
+        # Opens the specified file
+        file = open(file_name2)
 
-    # We split on anything that is not a comma, letter (case sensitive), or apostrophe
-    for line in file:
-        for token in re.split('[^a-zA-Z0-9\']', line):
+        # We split on anything that is not a comma, letter (case sensitive), or apostrophe
+        for line in file:
+            for token in re.split('[^a-zA-Z0-9\']', line):
 
-            # Multiple split chars in a row cause an empty token to be shown
-            if token:
-                if token.lower() in frequency_map and frequency_map[token.lower()] > 0:
-                    frequency_map[token.lower()] = -1
-                    total_intersections += 1
+                # Multiple split chars in a row cause an empty token to be shown
+                if token:
+                    if token.lower() in frequency_map and frequency_map[token.lower()] > 0:
+                        frequency_map[token.lower()] = -1
+                        total_intersections += 1
 
-                    # Prints the common tokens
-                    print(token.lower())
+                        # Prints the common tokens
+                        print(token.lower())
 
-    # Close the input stream
-    file.close()
+        # Close the input stream
+        file.close()
 
-    return total_intersections
+        return total_intersections
+
+    except:
+        print("Error! The following file does not exist:", file_name2)
+        raise
 
 if __name__ == '__main__':
 
-    # Starts the timer
-    start = clock()
+    # Argument counter
+    count = 1
 
-    print(determineIntersections("input2.txt", generateTokenFrequencyMap("input1.txt")))
-
-    # Ends the timer
-    end = clock()
-
-    # Displays execution time
-    print("Total Execution Time:", end - start, "sec")
+    # loops through all the input files
+    if len(sys.argv) < 3 or len(sys.argv) > 3:
+        print("Pass 2 input file as command line argument!")
+    else:
+        try:
+            print(determineIntersections(str(sys.argv[1]), generateTokenFrequencyMap(str(sys.argv[2]))))
+        except:
+            print("")
