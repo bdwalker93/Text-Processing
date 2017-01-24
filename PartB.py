6b56273f329db5576924772c56ed297a8d84ee24
bdwalker93 <bdwalker93@yahoo.com>
2017-01-23 22:56:37 -0800

    Fix the mistakes from previous commit of part a

--- PartB.py
+++ PartB.py
@@ -1,12 +1,12 @@
 import re
 
 # Reads input from the specified file and tokenizes all words within it
-def tokenize(file_name: str) -> [str]:
+def tokenize(file_name: str) -> {str, int}:
     # Opens the specified file
     file = open(file_name)
 
-    # Stores all of the words read in from the input file
-    words =  {}
+    # Stores all of the word frequencies in a map
+    frequency_map = {}
 
     # We split on anything that is not a comma, letter (case sensitive), or apostrophe
     for line in file:
@@ -14,16 +14,16 @@ def tokenize(file_name: str) -> [str]:
 
             # Multiple split chars in a row cause an empty token to be shown
             if token:
-                words.append(token.lower())
+                if token in frequency_map:
+                    frequency_map[token] += 1
+                else:
+                    frequency_map[token] = 1
 
-
-
-        print(len(words))
 
     # Close the input stream
     file.close()
 
-    return words
+    return frequency_map
 
 
 
